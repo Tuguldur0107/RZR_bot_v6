de5c175df01075a369cce9ed7758dc9e6f49c2a8
Tuguldur0107 <tuguldur.enkhtuvshin@yahoo.com>
2025-06-08 23:32:44 +0800

    Update RZR_bot_v6.0.py

--- RZR_bot_v6.0.py
+++ RZR_bot_v6.0.py
@@ -1474,6 +1474,7 @@ async def backup_now(interaction: discord.Interaction):
 # 🔄 Bot ажиллах үед
 @bot.event
 async def on_ready():
+    print(f"✅ [TEST] Logged in as {bot.user}")
     try:
         print(f"🤖 RZR Bot ажиллаж байна: {bot.user}")
         print("📁 Working directory:", os.getcwd())
@@ -1501,6 +1502,6 @@ async def on_ready():
 if __name__ == "__main__":
     keep_alive()  # 🔥 Render-д port нээж өгөх Flask-тай холбоос
     if TOKEN:
-        bot.run(TOKEN)
+        bot.run("MTM3NDA2NzA1MzY3MzA1NDI3MA.GA4gDl.v9ULHeU_6jvnE_dd2YI46_EIiWEqr03MZ5amVY")
     else:
         print("❌ DISCORD_TOKEN орчны хувьсагч тохируулаагүй байна.")
